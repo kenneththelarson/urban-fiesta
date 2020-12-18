dfee262af870c463d8297f39d484bd46c46d1b5f
Dave <bbec0689@gmail.com>
2020-12-17 21:30:31 -0700

    drinks api skeleton setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,46 @@
-function myFunction() {
-    fetch('https://cors-anywhere.herokuapp.com/')
+function getDrinkRecipe() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 
-    .then(function(randomeName) {
-        return randomeName;
-    })
-    .then(function(randomeName) {
-        console.log(randomeName)
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            getDrinkName(data);
+            getInstructions(data);
+            getIngredients(data);
+            getMeasure(data);
+        })
 }
 
+function getDrinkName(data) {
+    var drinks = data.drinks[0].strDrink;
+    // console.log(drinks)
+    
+}
+
+function getInstructions(data) {
+    var instructions = data.drinks[0].strInstructions;
+    // console.log(instructions)
+
+}
+
+
+function getIngredients(data) {
+    for (i = 1; i <= 15; i++) {
+
+        if (data.drinks[0][`strIngredient${i}`]) {
+            // console.log(data.drinks[0][`strIngredient${i}`])
+
+        }
+    }
+}
+
+function getMeasure(data) {
+    for (i = 1; i <= 15; i++) {
+
+        if (data.drinks[0][`strMeasure${i}`]) {
+            // console.log(data.drinks[0][`strMeasure${i}`])
+
+        }
+    }
+}
